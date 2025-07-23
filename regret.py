fac6ac0a15ebed049dcc838d6f7bc34d796c6ab7
mrSquidee <mr.squidy78@gmail.com>
2025-07-22 17:04:28 -0700

    rerun with all ballot permutations (m=4)

--- regret.py
+++ regret.py
@@ -2,38 +2,13 @@ from itertools import permutations
 
 ballots = list(permutations([0, 1, 2, 3]))
 
-# print(ballots)
-
-# print(len(ballots))
-
-# ballots = [
-#     [0,1,2,3],
-#     [1,2,3,0],
-#     [2,3,0,1],
-#     [3,0,1,2],
-# ]
-
-# ballots = [
-#     [0, 1, 2, 3],
-#     [1, 2, 0, 3],
-#     [2, 0, 1, 3],
-# ]
-
-# possible_utilities = [
-#     [0, 0, 0],
-#     [1, 0, 0],
-#     [0, 0, -1],
-# ]
-
 possible_utilities = [
-    [0,0,0,0],
-    [1,0,0,0],
-    # [0.5,0.5,0,0],
+    # [0,0,0,0],
+    # [1,0,0,0],
+    [0.5,0.5,0,0],
     [0,0,0,-1],
-    # [0,0,-0.5,-0.5]
+    [0,0,-0.5,-0.5]
 ]
-
-attempts = {}
 
 n = len(ballots)
 m = len(ballots[0])
@@ -55,13 +30,20 @@ def search(ballot_num, utility_profiles):
             profiles.clear()
         profiles.append(utility_profiles)
         return
+    
+    if ballots[ballot_num][0] == 0:
+        search(ballot_num + 1, utility_profiles + [(1, 0, 0, 0)])
+        return
+    elif ballots[ballot_num][-1] == 0:
+        search(ballot_num + 1, utility_profiles + [(0, 0, 0, 0)])
+        return
+
     for utility in possible_utilities:
         profile = [0] * m
+        
         for i, alt in enumerate(ballots[ballot_num]):
             profile[alt] = utility[i]
 
-        if profile[0] < 0:
-            continue
         search(ballot_num + 1, utility_profiles + [tuple(profile)])
 
 search(0, [])
@@ -72,19 +54,7 @@ regrets = {}
 
 print("Max regret:", max_regret)
 print("Profiles with max regret:")
-for profile in profiles:
+for profile in profiles[:5]:
     print(profile)
     utilities = [sum(profile[i][j] for i in range(n)) for j in range(m)]
-
-# for profile in attempts:
-#     utilities = attempts[profile]
-#     regret = (max(utilities) - (sum(utilities))/m )/n
-#     regrets[profile] = regret
-
-# worst_regret = max(regrets.values())
-# worst_cases = [profile for profile, regret in regrets.items() if regret == worst_regret]
-# print("Worst regret:", worst_regret)
-# print("Worst cases:")
-# for case in worst_cases:
-#     print(case)
-#     print("Utilities:", attempts[case])
+    print("Utilities:", utilities)
